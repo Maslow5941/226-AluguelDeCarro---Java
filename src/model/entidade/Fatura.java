07885b9e17ee966a19773a9233ebf27c2a4a17fc
Maslow5941 <maslow5941@hotmail.com>
2022-09-14 13:29:56 -0300

    Criando as classes

---
+++ src/model/entidade/Fatura.java
@@ -0,0 +1,38 @@
+package model.entidade;
+
+public class Fatura {
+
+	public Double pagamentoBasico;
+	public Double taxa;
+
+	public Fatura() {
+
+	}
+
+	public Fatura(Double pagamentoBasico, Double taxa) {
+
+		this.pagamentoBasico = pagamentoBasico;
+		this.taxa = taxa;
+	}
+
+	public Double getPagamentoBasico() {
+		return pagamentoBasico;
+	}
+
+	public void setPagamentoBasico(Double pagamentoBasico) {
+		this.pagamentoBasico = pagamentoBasico;
+	}
+
+	public Double getTaxa() {
+		return taxa;
+	}
+
+	public void setTaxa(Double taxa) {
+		this.taxa = taxa;
+	}
+	
+	public Double getPagamentoTotal() {
+		 return getPagamentoBasico()+getTaxa();
+	}
+
+}
---
+++ src/model/entidade/Veiculo.java
@@ -0,0 +1,28 @@
+package model.entidade;
+
+public class Veiculo {
+
+	
+	private String modelo;
+
+	public Veiculo() {
+		super();
+	}
+
+	public Veiculo(String modelo) {
+		
+		this.modelo = modelo;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	
+	
+	
+}
